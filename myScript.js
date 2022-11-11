98b6d669bb9343474817dd150c5f0aefbd21e0dc
Emily <easchoettler@gmail.com>
2022-11-11 15:40:02 -0500

    Basic JS file setup

---
+++ myScript.js
@@ -0,0 +1,17 @@
+const Gameboard = (() => { //module
+    const moves = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
+    return {displayController(moves)};
+})();
+
+const playerFactory = (name, symbol) => { //factory
+    return {name, symbol};
+}
+
+const playGame = (() => { //module
+
+})();
+
+const displayController = ((moves) => { //module
+    
+})();
+
