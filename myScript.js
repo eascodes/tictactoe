f280fe7f7552bb0f3da4b67603849fba934dc038
Emily <easchoettler@gmail.com>
2022-11-14 11:45:42 -0500

    Add functionality so X and O alternatively populate upon clicks on grid

--- myScript.js
+++ myScript.js
@@ -1,17 +1,56 @@
-const Gameboard = (() => { //module
-    const moves = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
-    return {displayController(moves)};
-})();
+
 
 const playerFactory = (name, symbol) => { //factory
-    return {name, symbol};
+    const getName = () => name;
+    const getSymbol = () => symbol;
+    return {name, symbol, getName, getSymbol};
 }
 
-const playGame = (() => { //module
+const playerOne = playerFactory("em", "X");
+const playerTwo = playerFactory("code", "O");
 
+const playGame = (() => { //module
+    let squares = Array.from(document.querySelectorAll(".square"));
+
+    function firstPlay() {
+        for (let i=0; i < 9; i++) {
+            squares[i].addEventListener("click", addX);
+        }
+        function addX() {
+            for (let i=0; i < 9; i++) {
+                squares[i].removeEventListener("click", addX);
+            }
+            let targetSquare = this;
+            let x = document.createElement("p");
+            x.textContent = "X";
+            targetMark = x;
+            targetSquare.appendChild(targetMark);
+            secondPlay();
+        }
+    } 
+
+    function secondPlay() {
+        for (let i=0; i < 9; i++) {
+            squares[i].addEventListener("click", addO);
+        }
+        function addO() {
+            for (let i=0; i < 9; i++) {
+                squares[i].removeEventListener("click", addO);
+            }
+            let targetSquare = this;
+            let o = document.createElement("p");
+            o.textContent = "O";
+            targetMark = o;
+            targetSquare.appendChild(targetMark);
+            firstPlay();
+        }
+    }
+
+    firstPlay();
 })();
 
-const displayController = ((moves) => { //module
-    
+const Gameboard = (() => { //module
+    const moves = [];
+    return {moves};
 })();
 
